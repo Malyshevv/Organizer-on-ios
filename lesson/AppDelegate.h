eb8ad932e59b3c9aa948e737867682bae2025b9f
Malyshev <malyshev.dev@ya.ru>
2017-06-03 14:58:23 +0300

    Initial Commit

---
+++ lesson/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lesson
+//
+//  Created by Malyshev on 03.06.17.
+//  Copyright © 2017 Malyshev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lesson/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lesson
+//
+//  Created by Malyshev on 03.06.17.
+//  Copyright © 2017 Malyshev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
